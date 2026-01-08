b66e258b55b58943234bb490b16978414f1bc439
navya-gupta-004 <151979145+navya-gupta-004@users.noreply.github.com>
2026-01-08 22:51:46 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+# TASK-1
+
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+
+Addition = float(a) + float(b)
+subtraction = float(a) - float(b)
+multiplication = float(a) * float(b)
+division = float(a) / float(b)
+
+print("Addition: ", Addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+# TASK-2
+
+first_name= input("Enter your first name:")
+last_name= input("Enter your last name:")
+full_name= first_name + " " + last_name
+print(f'hello {full_name} ! Welcome to Python programming.')
